594c2d37edce65316548e56428ed7f60ea198ab0
Jessica Thomas <cheddarcheesegremlin@gmail.com>
2026-03-29 23:54:00 -0400

    refactor: update HTML structure and add JavaScript for product display

---
+++ scripts/amazon.js
@@ -0,0 +1,92 @@
+console.log("Amazon script loaded");
+
+//saving the data in a data structure (array of objects) to make it easier to manipulate and display on the page
+const products = [{
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating:{
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090 //saved as cents to avoid floating point issues
+}, {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating:{
+        stars:4,
+        count: 127
+    },
+    priceCents: 2095
+},{
+    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        stars: 4.5,
+        count: 56
+    },
+    priceCents:799
+}];
+
+//generating HTML from data and inserting it into the page
+let productsHTML = '';
+
+
+//generating HTML from data
+
+products.forEach((product) => {
+    productsHTML += `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img
+              class="product-image"
+              src="${product.image}"
+            />
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img
+              class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
+            />
+            <div class="product-rating-count link-primary">${product.rating.count}</div>
+          </div>
+
+          <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png" />
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">Add to Cart</button>
+        </div>
+    `;
+    
+});
+console.log(productsHTML);
+
+//using DOM manipulation to insert the generated HTML into the page
+
+document.querySelector('.products-grid').innerHTML = productsHTML;
+//changing the html inside products-grid to new html
